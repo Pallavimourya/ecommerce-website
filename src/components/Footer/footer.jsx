977fa5145a5a65ee42b82bb9a09f3fab571161dc
pallavi mourya <pallavimourya99@gmail.com>
2025-03-26 17:59:17 +0530

    edit footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link from React Router
 import "./footer.css";
 import fb from "../../assets/img/fb.jpg";
 import insta from "../../assets/img/insta.jpg";
@@ -20,18 +21,10 @@ const Footer = () => {
         <div className="footer-section links">
           <h3>Quick Links</h3>
           <ul>
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-              <a href="/categories">Categories</a>
-            </li>
-            <li>
-              <a href="/specialoffers">Offers</a>
-            </li>
-            <li>
-              <a href="/contactus">Contact</a>
-            </li>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/categories">Categories</Link></li>
+            <li><Link to="/specialoffers">Offers</Link></li>
+            <li><Link to="/contactus">Contact</Link></li>
           </ul>
         </div>
 
@@ -39,51 +32,30 @@ const Footer = () => {
         <div className="footer-section support">
           <h3>Customer Support</h3>
           <ul>
-            <li>
-              <a href="/faqs">FAQs</a>
-            </li>
-            <li>
-              <a href="/returnpolicy">Return Policy</a>
-            </li>
-            <li>
-              <a href="/shippinginfo">Shipping Info</a>
-            </li>
-            <li>
-              <a href="/termsandconditions">Terms & Conditions</a>
-            </li>
+            <li><Link to="/faqs">FAQs</Link></li>
+            <li><Link to="/shippinginfo">Shipping Info</Link></li>
+            <li><Link to="/termsandconditions">Terms & Conditions</Link></li>
           </ul>
         </div>
 
         {/* ✅ Contact Section */}
         <div className="footer-section contact">
-        <h3>Contact Us</h3>
-         <p  style={{ color: "white" }}>Email: support@trendsfam.com</p>
-         <p  style={{ color: "white" }}>Phone: +91 98735 47210</p>
+          <h3>Contact Us</h3>
+          <p style={{ color: "white" }}>Email: support@trendsfam.com</p>
+          <p style={{ color: "white" }}>Phone: +91 98735 47210</p>
         </div>
 
         {/* ✅ Social Media */}
         <div className="footer-section social">
           <h3>Follow Us</h3>
           <div className="social-icons">
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
               <img src={fb} alt="Facebook" />
             </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
               <img src={insta} alt="Instagram" />
             </a>
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
               <img src={twitter} alt="Twitter" />
             </a>
           </div>
@@ -92,7 +64,7 @@ const Footer = () => {
 
       {/* ✅ Copyright */}
       <div className="footer-bottom">
-        <p  style={{ color: "black" }}>
+        <p style={{ color: "black" }}>
           &copy; {new Date().getFullYear()} YourEcommerce. All Rights Reserved.
         </p>
       </div>
